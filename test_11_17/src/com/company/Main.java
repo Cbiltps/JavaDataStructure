2c95492ba07a01a48d6277fceb91b0f27903ad9d
Cbiltps <1239669254@qq.com>
2021-11-17 02:51:50 +0800

    链表的中间结点

---
+++ test_11_17/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/**
+ * leetcode 的题 https://leetcode-cn.com/problems/middle-of-the-linked-list/
+ * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+ * 如果有两个中间结点，则返回第二个中间结点。
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
