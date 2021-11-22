19cf0979f30a97892971a26854531d3acbde5095
Cbiltps <1239669254@qq.com>
2021-11-22 16:58:32 +0800

    链表分割

---
+++ test2_11_22/src/com/company/Main.java
@@ -0,0 +1,54 @@
+/**
+ * 注意：这个是牛客网的题 题目直接看我下面的链接吧！
+ * 只是一个存代码的文档，复制到牛客可以运行！
+ * https://www.nowcoder.com/practice/0e27e0b064de4eacac178676ef9c9d70?tpId=8&&tqId=11004&rp=2&ru=/activity/oj&qru=/ta/cracking-the-coding-interview/question-ranking
+ */
+
+import java.util.*;
+
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Partition {
+    public ListNode partition(ListNode head, int x) {
+        ListNode bs = null;
+        ListNode be = null;
+        ListNode as = null;
+        ListNode ae = null;
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.val < x) {
+                if (bs == null) {
+                    bs = cur;
+                    be = cur;
+                }else {
+                    be.next = cur;
+                    be = be.next;
+                }
+            }else {
+                if (as == null) {
+                    as = cur;
+                    ae = cur;
+                }else {
+                    ae.next = cur;
+                    ae = ae.next;
+                }
+            }
+            cur = cur.next;
+        }
+        if (bs == null) {
+            return as;
+        }
+        be.next = as;
+        if (as != null) {
+            ae.next = null;
+        }
+        return bs;
+    }
+}
