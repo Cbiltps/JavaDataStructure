f4d29f553e6b0e81d3f200ba05bb663dc0d8a85a
Cbiltps <1239669254@qq.com>
2021-11-28 23:48:16 +0800

    双向链表（各种操作）

---
+++ test_11_28/src/com/company/MyLinkedList.java
@@ -0,0 +1,206 @@
+package com.company;
+
+class ListNode {
+    public int val;
+    public ListNode prev;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class MyLinkedList {
+
+    public ListNode head;//指向双向链表的头节点
+    public ListNode last;//指向的是尾巴节点
+
+    public void display() {
+        //和单链表的打印方式是一样的
+        ListNode cur = this.head;
+        while (cur != null) {
+            System.out.print(cur.val+" ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
+    //得到单链表的长度
+    public int size() {
+        int count = 0;//计数器
+        ListNode cur = this.head;
+        while (cur != null) {
+            count++;
+            cur = cur.next;
+        }
+        return count;
+    }
+
+    //查找是否包含关键字key是否在单链表当中
+    public boolean contains(int key){
+        ListNode cur = this.head;
+        while (cur != null) {
+            if (key == cur.val) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    //头插法
+    public void addFirst(int data) {
+        ListNode addNode = new ListNode(data);
+        if (this.head == null) {
+            this.head = addNode;
+            this.last = addNode;
+        }else {
+            addNode.next = this.head;
+            this.head.prev = addNode;
+            this.head = addNode;
+        }
+    }
+    //尾插法
+    public void addLast(int data){
+        ListNode addNode = new ListNode(data);
+        if (this.head == null) {
+            this.head = addNode;
+            this.last = addNode;
+        }else {
+            this.last.next = addNode;
+            addNode.prev = this.last;
+            this.last = addNode;
+        }
+
+    }
+
+    //寻找下标位置的结点
+    public ListNode searchIndex(int index) {
+        ListNode cur = this.head;
+        while (index != 0) {
+            cur = cur.next;
+            index--;
+        }
+        return cur;
+    }
+
+    //判断结点是否合法
+    public boolean isIndex(int index) {
+        if (index < 0 || index > size()) {
+            System.out.println("位置不合法！");
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    //任意位置插入,第一个数据节点为0号下标
+    public void addIndex(int index,int data) {
+        ListNode addNode = new ListNode(data);
+        if (!isIndex(index)) {
+            return;
+        }
+        if (index == 0) {
+            addFirst(data);
+        }else if(index == size()) {
+            addLast(data);
+        }else {
+            ListNode cur = searchIndex(index);
+            addNode.next = cur;
+            cur.prev.next = addNode;
+            addNode.prev = cur.prev;
+            cur.prev = addNode;
+        }
+    }
+
+    //删除第一次出现关键字为key的节点
+    public void remove(int key){
+        ListNode cur = this.head;
+
+//        while (cur != null) {
+//            if (cur.val == key) {
+//                if (cur == head) { //删除的结点是头结点
+//                    this.head = head.next;
+//                    this.head.prev = null;
+//                    break;
+//                }else if(cur == last){ //删除的结点是尾巴结点
+//                    cur.prev.next = cur.next;
+//                    last = last.prev;
+//                    break;
+//                }else { //删除的是中间的结点
+//                    cur.prev.next = cur.next;
+//                    cur.next.prev = cur.prev;
+//                    break;
+//                }
+//            }else {
+//                cur = cur.next;
+//            }
+//        }
+
+        //看看更高级的代码！
+        while (cur != null) {
+            if (cur.val == key) {
+                if (cur == head) { //删除头结点
+                    this.head = head.next;
+                    if (head != null) { //当只有一个节点的时候，head为空，会报错
+                        this.head.prev = null;
+                    }else {
+                        this.last = null;//这样就相当于删了
+                    }
+                }else {
+                    cur.prev.next = cur.next;
+                    if (cur.next != null) {
+                        cur.next.prev = cur.prev;//删除中间
+                    }else {
+                        this.last = last.prev;//删除尾巴
+                    }
+                }
+                return;
+            }
+            cur = cur.next;
+        }
+
+    }
+    //删除所有值为key的节点
+    public void removeAllKey(int key){
+        ListNode cur = this.head;
+        while (cur != null) {
+            if (cur.val == key) {
+                if (cur == head) { //删除头结点
+                    this.head = head.next;
+                    if (head != null) { //当只有一个节点的时候，head为空，会报错
+                        this.head.prev = null;
+                    }else {
+                        this.last = null;//这样就相当于删了
+                    }
+                }else {
+                    cur.prev.next = cur.next;
+                    if (cur.next != null) {
+                        cur.next.prev = cur.prev;//删除中间
+                    }else {
+                        this.last = last.prev;//删除尾巴
+                    }
+                }
+            }
+            cur = cur.next;
+        }
+    }
+
+
+    public void clear() {
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode curNext = head.next;
+            if(cur.next != null) {
+                cur.next = null;
+            }
+            if(cur.prev != null) {
+                cur.prev = null;
+            }
+            cur = cur.next;
+        }
+        head = null;
+        last = null;
+        System.out.println("null");
+    }
+}
---
+++ test_11_28/src/com/company/TestDemo.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class TestDemo {
+
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addLast(1);
+        myLinkedList.addLast(2);
+        myLinkedList.addLast(3);
+        myLinkedList.addLast(4);
+//        myLinkedList.addFirst(2);
+//        myLinkedList.addFirst(1);
+//        myLinkedList.addFirst(1);
+//        myLinkedList.addFirst(1);
+//        myLinkedList.addFirst(1);
+//        myLinkedList.addFirst(2);
+//        myLinkedList.addFirst(3);
+//        myLinkedList.addFirst(4);
+//        System.out.println(myLinkedList.size());
+//        myLinkedList.display();
+
+//        myLinkedList.addLast(9);
+//        System.out.println(myLinkedList.size());
+//        myLinkedList.display();
+
+//        System.out.println(myLinkedList.contains(3));
+
+//        myLinkedList.remove(3);
+//        myLinkedList.display();
+//        myLinkedList.remove(1);
+//        myLinkedList.display();
+//        myLinkedList.remove(9);
+//        myLinkedList.display();
+//        myLinkedList.removeAllKey(1);
+//        myLinkedList.display();
+//        myLinkedList.addIndex(5,99);
+//        myLinkedList.display();
+        myLinkedList.clear();
+    }
+}
