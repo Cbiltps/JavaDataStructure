c524ff228e36e170bac33cd2c9793e40e13568a5
Cbiltps <1239669254@qq.com>
2023-01-15 21:37:53 +0800

    堆的创建及基本操作

--- 2023_01_13 堆/src/TestDemo.java
+++ 2023_01_13 堆/src/TestDemo.java
@@ -1,8 +1,20 @@
 public class TestDemo {
+//    public static void main(String[] args) {
+//        //它默认是一个大堆还是小堆?
+//        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+//        // 没放置一个元素, 都要保证当前堆是大堆或者是小堆
+//        priorityQueue.add(15);
+//        priorityQueue.add(13);
+//        priorityQueue.add(20);
+//        System.out.println(priorityQueue.peek());// 验证默认是小堆
+//    }
+
     public static void main(String[] args) {
         int[] array = {27, 15, 19, 18, 28, 34, 65, 49, 25, 37};
         TestHeap testHeap = new TestHeap();
         testHeap.createHeap(array);
-        System.out.println("###################");
+        testHeap.offer(80);
+        System.out.println(testHeap.poll());
+        System.out.println("此处断点看数组是什么情况!");
     }
 }
--- 2023_01_13 堆/src/TestHeap.java
+++ 2023_01_13 堆/src/TestHeap.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class TestHeap {
     public int[] elem;
     public int usedSize;// 实际使用大小
@@ -9,7 +11,7 @@ public class TestHeap {
     /**
      * 向下调整函数的实现
      * @param parent 每棵树的根节点
-     * @param len 每棵树的调整的结束位置
+     * @param len 每棵树调整过的长度
      */
     public void shiftDown(int parent, int len) {
         int child = 2 * parent + 1;
@@ -30,7 +32,12 @@ public class TestHeap {
         }
     }
 
-    public  void createHeap(int[] array) {
+    /**
+     * 注意:这个方法的时间复杂度为O(n), 具体的过程看下面的链接.
+     * https://gitee.com/gaobo1/java-102/blob/master/102-%E6%9D%BF%E4%B9%A6/2022-01-22-%E4%BA%8C%E5%8F%89%E6%A0%91%E5%AE%8C+%E5%A0%86.png
+     * @param array
+     */
+    public void createHeap(int[] array) {
         for (int i = 0; i < array.length; i++) {
             elem[i] = array[i];
             usedSize++;
@@ -38,5 +45,61 @@ public class TestHeap {
         for (int parent = ((usedSize-1)-1)/2; parent >= 0; parent--) {
             shiftDown(parent, usedSize);
         }
+    }
+
+    /**
+     * 向上调整
+     * @param child 孩子的下标
+     */
+    private void shiftUp(int child) {
+        int parent = (child-1) / 2;
+        while (child > 0) {
+            if (elem[child] > elem[parent]) {
+                int tmp = elem[child];
+                elem[child] = elem[parent];
+                elem[parent] = tmp;
+                child = parent;
+                parent = (child-1) / 2;
+            } else {
+                break;
+            }
+        }
+    }
+
+    public void offer(int val) {
+        if(isFull()) {
+            //扩容
+            elem = Arrays.copyOf(elem,2*elem.length);
+        }
+        elem[usedSize++] = val;
+        //注意这里传入的是最后一个孩子的下标
+        shiftUp(usedSize-1);
+    }
+
+    public boolean isFull() {
+        return usedSize == elem.length;
+    }
+
+    public int poll() {
+        if (isEmpty()) {
+            throw new RuntimeException("优先级队列为空!");
+        }
+        int tmp = elem[0];
+        elem[0] = elem[usedSize-1];
+        elem[usedSize-1] = tmp;
+        usedSize--;
+        shiftDown(0, usedSize);
+        return tmp;
+    }
+
+    public boolean isEmpty() {
+        return usedSize == 0;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("优先级队列为空!");
+        }
+        return elem[0];
     }
 }
